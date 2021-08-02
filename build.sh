9eba9df4a8ac77381e9ccb3775fad028351d7b16
ZenithalHourlyRate <i@zenithal.me>
2021-08-03 00:14:56 +0800

    Fix symlink: cf pages not allowing symlink

--- build.sh
+++ build.sh
@@ -12,29 +12,39 @@ yarn --frozen-lockfile
 yarn build
 cd ../
 
-## render list
+## generate mirrors list
 cd list && node generate.js && cd ..
 
 ## render legacy page for each province
 rm -rf dist && mkdir -p dist
+
+function render() {
+  ## no $1 nor $2: render for webroot
+  ## $1 and no $2: render for province with its own conf
+  ## $1 '' and $2: render for province with global conf
+
+  cp list/${1}mirrors.js mirrorz/src/config/mirrors.js
+  cd mirrorz && yarn legacy_build && cd ../
+  if [ "$#" -lt 1 ]; then
+    cp -r mirrorz/dist/* dist
+  else
+    cp -r mirrorz/dist dist/${1}${2}
+  fi
+  cp dist/${1}${2}_/index.html dist/${1}${2}index.html
+  rm -r mirrorz/dist/_/
+}
+
 for province in {bj,tj,sh,cq,he,ha,sx,nm,ln,jl,hl,js,zj,ah,fj,jx,sd,hb,hn,gd,gx,hi,sc,gz,yn,xz,sn,gs,qh,nx,xj,tw,hk,mo}; do
   echo Building for $province
   if [ -e list/$province ]; then
-    cp list/$province/mirrors.js mirrorz/src/config/mirrors.js
-    cd mirrorz && yarn legacy_build && cd ../
-    cp -r mirrorz/dist dist/$province
-    cp dist/$province/_/index.html dist/$province/index.html
-    rm -r mirrorz/dist/_/
+    render $province/
   else
-    ln -s ../dist dist/$province
+    render '' $province/
   fi
 done
 
-# build for mirrors-cn.pages.dev and some mirrors.pr.cn that has no mirrors
-cp list/mirrors.js mirrorz/src/config/mirrors.js
-cd mirrorz && yarn legacy_build && cd ../
-cp -r mirrorz/dist/* dist
-cp dist/_/index.html dist/index.html
+# build for mirrors-cn.pages.dev
+render
 
 # cleanup
 cd mirrorz && git reset --hard && cd ..
